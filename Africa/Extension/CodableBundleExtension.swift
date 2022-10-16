9111223152072fe1f58b1dc6cdf5a23d60a20d88
Munashif Muhsin <munashif.muhsin@gmail.com>
2022-10-16 15:04:05 +0400

    added cover images from json with json decoder

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -11,5 +11,26 @@ extension Bundle {
     
     func decode(_ file: String) -> [CoverImage] {
         
+        // 1. Locate the JSON file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        // 2. Create a property for data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for Decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode data from \(file)")
+        }
+        
+        // 5. Return the data
+        return loaded
+        
     }
 }
