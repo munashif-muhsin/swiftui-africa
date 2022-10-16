4a87d677078ab28e828606f14a01ad74f25aa4f3
Munashif Muhsin <munashif.muhsin@gmail.com>
2022-10-16 16:06:09 +0400

    Added Animals list to content view screen

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    let animals: [Animal] = Bundle.main.decode("Data/animals.json")
     var body: some View {
         
         NavigationView {
@@ -16,7 +17,10 @@ struct ContentView: View {
                     .frame(height: 300)
                     .listRowInsets(
                         EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                    )
+                    ).padding(.bottom, 12)
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             }
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Muhsin Munashif on 16/10/2022.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
